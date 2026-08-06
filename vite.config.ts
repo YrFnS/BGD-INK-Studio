b041df7a3dc0bbf17a252676bd6c19fdc7da805d
Yasser <116118149+YrFnS@users.noreply.github.com>
2026-08-06 22:14:10 +0300

    build: integrate Tailwind with Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
 import { fileURLToPath, URL } from 'node:url';
-import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite';
 import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
   server: {
     port: 3000,
     host: '0.0.0.0',
   },
-  plugins: [react()],
+  plugins: [tailwindcss(), react()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('.', import.meta.url)),
